53bd8881849d12f617c5cd8f2de16f0d822d0304
Shantanu-21 <shantanumogre21@gmail.com>
2022-06-24 12:47:15 +0530

    US Sprint 1 create git repository and push project on git

---
+++ src/main/java/com/Shantanu/UserManegmentApp/UserManegmentAppApplication.java
@@ -0,0 +1,13 @@
+package com.Shantanu.UserManegmentApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserManegmentAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserManegmentAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Shantanu/UserManegmentApp/UserManegmentAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Shantanu.UserManegmentApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserManegmentAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
