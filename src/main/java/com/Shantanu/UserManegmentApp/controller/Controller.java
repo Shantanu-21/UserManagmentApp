22ae268e9799121516e98e836cda5f102fdc83bf
Shantanu-21 <shantanumogre21@gmail.com>
2022-06-25 01:56:44 +0530

    US Sprint 2 Create Rest Controllers required to handle request & response

---
+++ src/main/java/com/Shantanu/UserManegmentApp/controller/Controller.java
@@ -0,0 +1,12 @@
+package com.Shantanu.UserManegmentApp.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.Shantanu.UserManegmentApp.service.ServiceIMPL;
+
+@RestController
+public class Controller {
+@Autowired
+	private ServiceIMPL serviceIMPL;
+}
--- src/main/java/com/Shantanu/UserManegmentApp/service/ServiceIMPL.java
+++ src/main/java/com/Shantanu/UserManegmentApp/service/ServiceIMPL.java
@@ -1,8 +1,23 @@
 package com.Shantanu.UserManegmentApp.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.Shantanu.UserManegmentApp.repositories.CitiesRepositories;
+import com.Shantanu.UserManegmentApp.repositories.Conutryrepositories;
+import com.Shantanu.UserManegmentApp.repositories.StatesRepositories;
+import com.Shantanu.UserManegmentApp.repositories.UserAccountrepositories;
 
 @Service
 public class ServiceIMPL implements ServiceI {
+
+	@Autowired
+	private CitiesRepositories citiesRepositories;
+	@Autowired
+	private StatesRepositories statesRepositories;
+	@Autowired
+	private Conutryrepositories conutryrepositories;
+    @Autowired
+	private UserAccountrepositories userAccountrepositories;
 
 }
